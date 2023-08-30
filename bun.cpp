faeb771d083a6a936ae1ef87085dbfcca87caab5
Daniel L. Lu <daniel@lawrence.lu>
2023-08-30 11:15:43 -0700

    start implementing multiple buns

--- bun.cpp
+++ bun.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <array>
 #include <chrono>
+#include <functional>
 #include <iostream>
 #include <queue>
 #include <string>
@@ -8,45 +9,61 @@
 #include <unordered_set>
 #include <vector>
 
-class Level;
-
-struct state {
-    int paq;
-    int bun;
-    std::vector<int> hist;
-
-    int64_t as_int64() const { return paq + (15 * 9 * bun); }
-};
-
-class Level {
+class Game {
    public:
     static constexpr int width = 15;
     static constexpr int height = 9;
     // left, down, right, up
-    const std::array<int, 4> directions{-1, Level::width, 1, -Level::width};
+    static constexpr std::array<int, 4> directions{-1, Game::width, 1,
+                                                   -Game::width};
+    static int xy_ind(int x, int y) { return x + Game::width * y; }
+};
+
+class Level;
+
+struct state {
+    int paq;
+    std::vector<int> buns;
+    std::vector<int> bunstacks;
+    std::vector<int> hist;
+
+    Level *level;
+
+    int64_t key() const {
+        int64_t x = paq;
+        for (int i = 0; i < 4; i++) {
+            x *= Game::width * Game::height;
+            if (i < buns.size()) {
+                x += buns[i];
+            }
+        }
+        return x;
+    }
+};
+
+class Level {
+   public:
     state init_state;
+    std::string world;
 
    private:
-    std::string world;
-    std::array<int, Level::width * Level::height> degree;
-    std::array<int, Level::width * Level::height> dead_end;
-    std::array<int, Level::width * Level::height * Level::width * Level::height>
+    std::array<int, Game::width * Game::height> degree;
+    std::array<int, Game::width * Game::height> dead_end;
+    std::array<int, Game::width * Game::height * Game::width * Game::height>
         bun_move_memo;
 
    public:
-    int xy_ind(int x, int y) { return x + width * y; }
-    Level(const std::string &world_)
-        : world{world_}, init_state{.paq = -1, .bun = -1} {
-        for (int y = 1; y < height - 1; y++) {
-            for (int x = 1; x < width - 1; x++) {
-                const int ind = xy_ind(x, y);
+    Level(const std::string &world_) : init_state{.paq = -1}, world{world_} {
+        for (int y = 1; y < Game::height - 1; y++) {
+            for (int x = 1; x < Game::width - 1; x++) {
+                const int ind = Game::xy_ind(x, y);
 
                 if (world[ind] == 'p') {
                     init_state.paq = ind;
                     world[ind] = ' ';
                 }
                 if (world[ind] == 'b') {
-                    init_state.bun = ind;
+                    init_state.buns.push_back(ind);
                     world[ind] = ' ';
                 }
             }
@@ -54,9 +71,10 @@ class Level {
         if (init_state.paq == -1) {
             std::cerr << "paq not found" << std::endl;
         }
-        if (init_state.bun == -1) {
+        if (init_state.buns.size() == 0) {
             std::cerr << "bun not found" << std::endl;
         }
+        init_state.level = this;
         precompute_degrees();
         precompute_dead_ends();
         std::fill(bun_move_memo.begin(), bun_move_memo.end(), -1);
@@ -64,13 +82,14 @@ class Level {
 
     void precompute_degrees() {
         std::fill(degree.begin(), degree.end(), 0);
-        for (int y = 1; y < height - 1; y++) {
-            for (int x = 1; x < width - 1; x++) {
-                degree[xy_ind(x, y)] = 0;
-                if (world[xy_ind(x, y)] == ' ') {
+        for (int y = 1; y < Game::height - 1; y++) {
+            for (int x = 1; x < Game::width - 1; x++) {
+                const int ind = Game::xy_ind(x, y);
+                degree[ind] = 0;
+                if (world[ind] == ' ') {
                     for (int dir = 0; dir < 4; dir++) {
-                        if (world[xy_ind(x, y) + directions[dir]] == ' ') {
-                            degree[xy_ind(x, y)]++;
+                        if (world[ind + Game::directions[dir]] == ' ') {
+                            degree[ind]++;
                         }
                     }
                 }
@@ -80,35 +99,36 @@ class Level {
 
     void precompute_dead_ends() {
         std::fill(dead_end.begin(), dead_end.end(), 0);
-        for (int y = 1; y < height - 1; y++) {
-            for (int x = 1; x < width - 1; x++) {
-                int ind = xy_ind(x, y);
+        for (int y = 1; y < Game::height - 1; y++) {
+            for (int x = 1; x < Game::width - 1; x++) {
+                int ind = Game::xy_ind(x, y);
                 if (degree[ind] != 1) {
                     continue;
                 }
                 int dead_end_dir = 0;
                 for (; dead_end_dir < 4; dead_end_dir++) {
-                    if (world[ind + directions[dead_end_dir]] == ' ') {
+                    if (world[ind + Game::directions[dead_end_dir]] == ' ') {
                         break;
                     }
                 }
                 do {
                     dead_end[ind] = 1;
-                    ind += directions[dead_end_dir];
+                    ind += Game::directions[dead_end_dir];
                 } while (degree[ind] == 2);
             }
         }
     }
 
-    int bun_move(state s) {
-        const state original_s = s;
-        if (bun_move_memo[s.as_int64()] != -1) {
-            return bun_move_memo[s.as_int64()];
+    int bun_move(const int paq, const int original_bun) {
+        int bun = original_bun;
+        const int memo_ind = paq + Game::width * Game::height * original_bun;
+        if (bun_move_memo[memo_ind] != -1) {
+            return bun_move_memo[memo_ind];
         }
 
         bool done = false;
         while (!done) {
-            const int distance = s.paq - s.bun;
+            const int distance = paq - bun;
             int dir = 0;
             switch (distance) {
                 case 2:
@@ -121,13 +141,13 @@ class Level {
                     // bun goes right
                     dir = 2;
                     break;
-                case 2 * width:
-                case width:
+                case 2 * Game::width:
+                case Game::width:
                     // bun goes up
                     dir = 3;
                     break;
-                case -2 * width:
-                case -width:
+                case -2 * Game::width:
+                case -Game::width:
                     // bun goes down
                     dir = 1;
                     break;
@@ -135,173 +155,150 @@ class Level {
                     // bun does nothing
                     done = true;
             }
-            for (int dd : {2, -2, 2 * width, -2 * width}) {
-                if (distance == dd && world[s.bun + dd / 2] != ' ') {
+            for (int dd : {2, -2, 2 * Game::width, -2 * Game::width}) {
+                if (distance == dd && world[bun + dd / 2] != ' ') {
                     done = true;
                     break;
                 }
             }
             if (done) break;
+
             do {
                 const int original_dir = dir;
-                // std::cerr << "original_dir " << original_dir << std::endl;
                 bool works = false;
-                int new_bun = s.bun + directions[dir];
-                // choose one of the three directions if it's available and not
-                // a dead end
-                if (world[new_bun] == ' ' && !dead_end[new_bun]) {
-                    // std::cerr << "going forward " << dir << std::endl;
-                    works = true;
-                }
-                if (!works) {
-                    // turn left
-                    dir = (original_dir + 1) % 4;
-                    new_bun = s.bun + directions[dir];
-                    if (world[new_bun] == ' ' && !dead_end[new_bun]) {
-                        // std::cerr << "going left " << dir << std::endl;
-                        works = true;
-                    }
-                }
-                if (!works) {
-                    // turn right
-                    dir = (original_dir + 3) % 4;
-                    new_bun = s.bun + directions[dir];
-                    if (world[new_bun] == ' ' && !dead_end[new_bun]) {
-                        // std::cerr << "going right " << dir << std::endl;
-                        works = true;
-                    }
-                }
-                // choose one of the three directions, even if it's a dead end
-                if (!works) {
-                    dir = original_dir;
-                    new_bun = s.bun + directions[dir];
-                    if (world[new_bun] == ' ') {
-                        // std::cerr << "going forward no dead end " << dir <<
-                        // std::endl;
-                        works = true;
-                    }
-                }
-                if (!works) {
-                    // turn left
-                    dir = (original_dir + 1) % 4;
-                    new_bun = s.bun + directions[dir];
-                    if (world[new_bun] == ' ') {
-                        // std::cerr << "going left no dead end " << dir <<
-                        // std::endl;
-                        works = true;
-                    }
-                }
-                if (!works) {
-                    // turn right
-                    dir = (original_dir + 3) % 4;
-                    new_bun = s.bun + directions[dir];
-                    if (world[new_bun] == ' ') {
-                        // std::cerr << "going right no dead end " << dir <<
-                        // std::endl;
-                        works = true;
+
+                const std::array<std::function<bool(int)>, 3> predicates = {
+                    [&](int b) -> bool {
+                        return world[b] == ' ' && !dead_end[b];
+                    },
+                    [&](int b) -> bool { return world[b] == ' '; },
+                    [&](int b) -> bool { return world[b] == 'o'; }};
+
+                int new_bun = 0;
+                for (const auto &predicate : predicates) {
+                    // choose one of the three directions if it's available
+                    // straight, left, right
+                    for (const int delta_dir : {0, 1, 3}) {
+                        if (!works) {
+                            dir = (original_dir + delta_dir) % 4;
+                            new_bun = bun + Game::directions[dir];
+                            if (predicate(new_bun)) {
+                                works = true;
+                                break;
+                            }
+                        }
                     }
                 }
 
                 if (works) {
                     do {
-                        new_bun = s.bun + directions[dir];
+                        new_bun = bun + Game::directions[dir];
                         if (world[new_bun] != ' ') {
                             break;
                         }
-                        s.bun = new_bun;
+                        bun = new_bun;
                     } while (degree[new_bun] == 2 || degree[new_bun] == 1);
                 } else {
                     done = true;
                     break;
                 }
-            } while (bun_threatened(s.paq - s.bun));
+            } while (bun_threatened(paq - bun));
         }
-        bun_move_memo[original_s.as_int64()] = s.bun;
-        /*
-        if (s.bun != original_s.bun) {
-            std::cerr << "moving bun!!!" << std::endl;
-            viz(original_s);
-            viz(s);
-            std::cerr << "moved successfully" << std::endl;
-        }
-        */
-        return s.bun;
+
+        bun_move_memo[memo_ind] = bun;
+        return bun;
     }
 
     bool bun_threatened(int distance) {
-        const std::array<int, 8> threats{2,         1,     -2,         -1,
-                                         2 * width, width, -2 * width, -width};
+        const std::array<int, 8> threats{2,
+                                         1,
+                                         -2,
+                                         -1,
+                                         2 * Game::width,
+                                         Game::width,
+                                         -2 * Game::width,
+                                         -Game::width};
         return std::find(std::begin(threats), std::end(threats), distance) !=
                std::end(threats);
     }
+};
 
-    state bfs(state s) {
-        std::queue<state> q;
-        q.push(s);
-        std::unordered_set<int64_t> visited;
-        while (!q.empty()) {
-            state s = q.front();
-            // viz(s);
-            if (s.paq == s.bun) {
-                return s;
-            }
-            q.pop();
-            for (int dir = 0; dir < 4; dir++) {
-                state ns{.paq = s.paq + directions[dir], .bun = s.bun};
-                if (world[ns.paq] != ' ') {
-                    continue;
-                }
-                ns.bun = bun_move(ns);
-                const int64_t ns_key = ns.as_int64();
-                if (visited.count(ns_key) != 0) {
-                    continue;
-                }
-                visited.insert(ns_key);
-                ns.hist = s.hist;
-                ns.hist.push_back(dir);
-                q.push(ns);
-            }
-        }
-        std::cerr << "no solution found" << std::endl;
-        return s;
+void viz(const state &s) {
+    std::string world = s.level->world;
+    world[s.paq] = 'p';
+    for (const int bun : s.buns) {
+        world[bun] = 'b';
     }
-
-    void viz_history(const state &s) {
-        state s2 = init_state;
-        for (const int d : s.hist) {
-            viz(s2);
-            s2.paq += directions[d];
-            s2.bun = bun_move(s2);
-        }
-        viz(s);
-    }
-
-    void viz(const state &s) {
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                const int ind = xy_ind(x, y);
-                if (ind == s.paq) {
-                    std::cerr << "p";
-                } else if (ind == s.bun) {
-                    std::cerr << "b";
-                } else {
-                    std::cerr << world[ind];
-                }
-            }
-            std::cerr << std::endl;
-        }
-        for (const int h : s.hist) {
-            std::cerr << "asdw"[h];
+    for (int y = 0; y < Game::height; y++) {
+        for (int x = 0; x < Game::width; x++) {
+            const int ind = Game::xy_ind(x, y);
+            std::cerr << world[ind];
         }
         std::cerr << std::endl;
     }
-
-    bool edge(int p) {
-        int x = p % width;
-        int y = p / width;
-        return x == 0 || x == width - 1 || y == 0 || y == height - 1;
+    for (const int h : s.hist) {
+        std::cerr << "<v>^"[h];
     }
-};
+    std::cerr << std::endl;
+}
+
+void viz_history(state init_s, const state &s) {
+    for (const int d : s.hist) {
+        viz(init_s);
+
+        init_s.paq += Game::directions[d];
+        std::vector<int> new_buns;
+        for (const int bun : init_s.buns) {
+            int new_bun = init_s.level->bun_move(init_s.paq, bun);
+            new_buns.push_back(new_bun);
+        }
+        init_s.buns = new_buns;
+    }
+    viz(s);
+}
+
+state bfs(state s) {
+    std::queue<state> q;
+    q.push(s);
+    std::unordered_set<int64_t> visited;
+    while (!q.empty()) {
+        const state s = q.front();
+        q.pop();
+        // viz(s);
+        bool finished = true;
+        for (const int bun  : s.buns) {
+            if (s.paq != bun) {
+                finished = false;
+            }
+        }
+        if (finished) {
+            return s;
+        }
+        for (int dir = 0; dir < 4; dir++) {
+            const int new_paq = s.paq + Game::directions[dir];
+            if (s.level->world[new_paq] != ' ') {
+                continue;
+            }
+            std::vector<int> new_buns;
+            for (const int bun : s.buns) {
+                int new_bun = s.level->bun_move(new_paq, bun);
+                new_buns.push_back(new_bun);
+            }
+
+            state ns{.paq = new_paq, .buns = new_buns, .level = s.level};
+            const int64_t ns_key = ns.key();
+            if (visited.count(ns_key) != 0) {
+                continue;
+            }
+            visited.insert(ns_key);
+            ns.hist = s.hist;
+            ns.hist.push_back(dir);
+            q.push(ns);
+        }
+    }
+    std::cerr << "no solution found" << std::endl;
+    return s;
+}
 
 int main() {
     /*
@@ -319,6 +316,42 @@ int main() {
     /*
     const std::string world =
         "###############"
+        "###############"
+        "####o###     ##"
+        "#  # ### # # ##"
+        "#      b     ##"
+        "#  # ##### ####"
+        "####p####   ###"
+        "########## ####"
+        "###############";
+        */
+    /*
+    const std::string world =
+        "###############"
+        "############# #"
+        "####p  #      #"
+        "#### # # ## ###"
+        "####   b  # ###"
+        "######## ##  o#"
+        "########    ###"
+        "###############"
+        "###############";
+        */
+    /*
+    const std::string world =
+        "###############"
+        "#####     #####"
+        "#o    # #b    #"
+        "#####     ### #"
+        "####  # #   # #"
+        "#####     # #p#"
+        "### ### ### ###"
+        "###          ##"
+        "###############";
+        */
+    /*
+    const std::string world =
+        "###############"
         "##    ##   #  #"
         "#p   b   #  o##"
         "####  ##   #  #"
@@ -330,6 +363,17 @@ int main() {
     */
     const std::string world =
         "###############"
+        "#     o#  #####"
+        "#   #### ###p##"
+        "##  ####     ##"
+        "#          #  #"
+        "#   ###  #    #"
+        "## #   ##  b# #"
+        "##b     #     #"
+        "###############";
+    /*
+    const std::string world =
+        "###############"
         "#  #    ##### #"
         "#           # #"
         "###### ####   #"
@@ -338,12 +382,14 @@ int main() {
         "#### ##       #"
         "###  ########p#"
         "###############";
+        */
     Level level(world);
     // level.bun_move(state{.paq=39, .bun=37});
 
     auto start = std::chrono::steady_clock::now();
 
-    state s = level.bfs(level.init_state);
+    state init_s = level.init_state;
+    state s = bfs(init_s);
 
     auto end = std::chrono::steady_clock::now();
 
@@ -352,5 +398,5 @@ int main() {
 
     std::cout << "Elapsed time: " << elapsed.count() << " microseconds"
               << std::endl;
-    level.viz_history(s);
+    viz_history(init_s, s);
 }
